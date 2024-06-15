ee07211cd8a6bfa422559a4e08a188ac9a20d732
J-Lw <JLaw0@proton.me>
2024-06-15 20:05:07 -0300

    Checkpoint.

--- ex/gg/src/main.rs
+++ ex/gg/src/main.rs
@@ -1,12 +1,12 @@
 use std::io; 
 
-// Obtain and return input from user.
+// Obtain and return input.
 fn get_input() -> String {
     let mut input = String::new();
 
     println!("Awaiting input:"); 
 
-    // Read user entry into 'input'.
+    // Read entry into 'input'.
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read user input.");
@@ -14,19 +14,40 @@ fn get_input() -> String {
     input 
 }
 
+// Check if the guess is the same as the specified target.
 fn check_guess(g: String, t: String) -> bool {
-    if g == t {
-        println!("test: true");
+    if g.trim() == t {
+        println!("Correct!");
         return true;
     }
 
-    println!("test: true");
     false
+}
+
+fn analyze_guess(g: String, t: String) {
+    for (index, char) in g.iter().enumerate() {
+        
+    }
 }
 
 fn main() {
     let target: String = String::from("target");
 
-    let guess: String = get_input();
-    check_guess(guess, target);
+    for n in 0..=2 { 
+        let guess: String = get_input();
+
+        if check_guess(guess, target) {
+            return;
+        }
+    }
+
+    loop {
+        let guess: String = get_input();
+
+        if check_guess(guess, target) {
+            return;
+        }
+
+        analyze_guess(guess, target);
+    }
 }
